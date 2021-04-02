10effe727de5658c65cd7eb1a09e72297e68f4e2
Kevwe Uwotu <kevwe1996@gmail.com>
2021-04-02 09:52:38 +0100

    etch basic functionality

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
 let container = document.querySelector("#container");
 
-for (i = 0; i < 256; i++) {
-  const grid = document.createElement("div");
-  grid.classList.add("squares");
+let input = 14;
 
-  changeColor = () => {
-    grid.style.cssText = "background-color: pink";
-  };
+function random_bg_color() {
+  var x = Math.floor(Math.random() * 256);
+  var y = Math.floor(Math.random() * 256);
+  var z = Math.floor(Math.random() * 256);
+  var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+  return bgColor;
+}
 
-  grid.addEventListener("mouseenter", changeColor);
+function etch(input) {
+  for (i = 0; i < input ** 2; i++) {
+    container.style.cssText = `grid-template-columns: repeat(${input}, 1fr); grid-template-rows: repeat(${input}, 1fr`;
 
-  container.appendChild(grid);
+    const grid = document.createElement("div");
+    grid.classList.add("squares");
+
+    const rainbow = random_bg_color();
+
+    changeColor = () => {
+      grid.style.cssText = `background-color: ${rainbow}`;
+    };
+
+    grid.addEventListener("mouseenter", changeColor);
+
+    container.appendChild(grid);
+  }
+}
+
+removeGrid = () => {
+  for (i = 0; i < input ** 2; i++) {
+    let selectGrid = document.querySelector(".squares");
+    container.removeChild(selectGrid);
+  }
+};
+
+function refresh() {
+  removeGrid();
+  let howManySquares = prompt("How many sqaures per side?", "");
+  let newInput = Number(howManySquares);
+  input = newInput;
+  etch(input);
 }
 
 let restartbtn = document.querySelector(".restart-button");
 
-function refresh() {
-  window.location.reload();
-}
-
 restartbtn.addEventListener("click", refresh);
+
+etch(input);
